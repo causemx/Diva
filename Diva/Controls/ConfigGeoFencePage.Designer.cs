52885ce042265fa38cdf8feda3833ac7b4ed74cb
causemx <itri990380@itri.org.tw>
2018-10-25 16:46:02 +0800

    modify big5 in geofenceconfig.

--- Diva/Controls/ConfigGeoFencePage.Designer.cs
+++ Diva/Controls/ConfigGeoFencePage.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfigGeoFencePage));
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
@@ -59,9 +60,7 @@
 			// 
 			// tableLayoutPanel1
 			// 
-			this.tableLayoutPanel1.ColumnCount = 2;
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+			resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
 			this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
 			this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
 			this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
@@ -74,255 +73,154 @@
 			this.tableLayoutPanel1.Controls.Add(this.label6, 0, 5);
 			this.tableLayoutPanel1.Controls.Add(this.NumericUpDown_Fence_RTL_Altitude, 1, 5);
 			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-			this.tableLayoutPanel1.Location = new System.Drawing.Point(36, 54);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-			this.tableLayoutPanel1.RowCount = 7;
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(350, 168);
-			this.tableLayoutPanel1.TabIndex = 0;
 			// 
 			// label2
 			// 
-			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			resources.ApplyResources(this.label2, "label2");
 			this.label2.ForeColor = System.Drawing.Color.White;
-			this.label2.Location = new System.Drawing.Point(3, 30);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(45, 18);
-			this.label2.TabIndex = 1;
-			this.label2.Text = "Type";
 			// 
 			// label3
 			// 
-			this.label3.AutoSize = true;
-			this.label3.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			resources.ApplyResources(this.label3, "label3");
 			this.label3.ForeColor = System.Drawing.Color.White;
-			this.label3.Location = new System.Drawing.Point(3, 56);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(53, 18);
-			this.label3.TabIndex = 2;
-			this.label3.Text = "Action";
 			// 
 			// label4
 			// 
-			this.label4.AutoSize = true;
-			this.label4.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			resources.ApplyResources(this.label4, "label4");
 			this.label4.ForeColor = System.Drawing.Color.White;
-			this.label4.Location = new System.Drawing.Point(3, 82);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(92, 18);
-			this.label4.TabIndex = 3;
-			this.label4.Text = "Max Radius";
 			// 
 			// label5
 			// 
-			this.label5.AutoSize = true;
-			this.label5.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			resources.ApplyResources(this.label5, "label5");
 			this.label5.ForeColor = System.Drawing.Color.White;
-			this.label5.Location = new System.Drawing.Point(3, 110);
 			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(101, 18);
-			this.label5.TabIndex = 4;
-			this.label5.Text = "Max Altitude";
 			// 
 			// CheckBox_Enable
 			// 
-			this.CheckBox_Enable.Enabled = false;
-			this.CheckBox_Enable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.CheckBox_Enable.Location = new System.Drawing.Point(143, 3);
+			resources.ApplyResources(this.CheckBox_Enable, "CheckBox_Enable");
 			this.CheckBox_Enable.Name = "CheckBox_Enable";
 			this.CheckBox_Enable.OffValue = 0D;
 			this.CheckBox_Enable.OnValue = 1D;
-			this.CheckBox_Enable.Padding = new System.Windows.Forms.Padding(3);
 			this.CheckBox_Enable.ParamName = null;
-			this.CheckBox_Enable.Size = new System.Drawing.Size(63, 24);
-			this.CheckBox_Enable.TabIndex = 5;
-			this.CheckBox_Enable.Text = "myCheckBox1";
 			this.CheckBox_Enable.UseVisualStyleBackColor = true;
 			// 
 			// ComboBox_Fence_Type
 			// 
+			resources.ApplyResources(this.ComboBox_Fence_Type, "ComboBox_Fence_Type");
 			this.ComboBox_Fence_Type.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.ComboBox_Fence_Type.Enabled = false;
 			this.ComboBox_Fence_Type.FormattingEnabled = true;
-			this.ComboBox_Fence_Type.Location = new System.Drawing.Point(143, 33);
 			this.ComboBox_Fence_Type.Name = "ComboBox_Fence_Type";
 			this.ComboBox_Fence_Type.ParamName = null;
-			this.ComboBox_Fence_Type.Size = new System.Drawing.Size(160, 20);
 			this.ComboBox_Fence_Type.SubControl = null;
-			this.ComboBox_Fence_Type.TabIndex = 6;
 			// 
 			// ComboBox_Fence_Action
 			// 
+			resources.ApplyResources(this.ComboBox_Fence_Action, "ComboBox_Fence_Action");
 			this.ComboBox_Fence_Action.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.ComboBox_Fence_Action.Enabled = false;
 			this.ComboBox_Fence_Action.FormattingEnabled = true;
-			this.ComboBox_Fence_Action.Location = new System.Drawing.Point(143, 59);
 			this.ComboBox_Fence_Action.Name = "ComboBox_Fence_Action";
 			this.ComboBox_Fence_Action.ParamName = null;
-			this.ComboBox_Fence_Action.Size = new System.Drawing.Size(160, 20);
 			this.ComboBox_Fence_Action.SubControl = null;
-			this.ComboBox_Fence_Action.TabIndex = 7;
 			// 
 			// NumericUpDown_Fence_Radius
 			// 
-			this.NumericUpDown_Fence_Radius.Enabled = false;
-			this.NumericUpDown_Fence_Radius.Location = new System.Drawing.Point(143, 85);
+			resources.ApplyResources(this.NumericUpDown_Fence_Radius, "NumericUpDown_Fence_Radius");
 			this.NumericUpDown_Fence_Radius.Max = 1F;
 			this.NumericUpDown_Fence_Radius.Min = 0F;
 			this.NumericUpDown_Fence_Radius.Name = "NumericUpDown_Fence_Radius";
 			this.NumericUpDown_Fence_Radius.ParamName = null;
-			this.NumericUpDown_Fence_Radius.Size = new System.Drawing.Size(160, 22);
-			this.NumericUpDown_Fence_Radius.TabIndex = 8;
 			// 
 			// NumericUpDown_Fence_Altitude
 			// 
-			this.NumericUpDown_Fence_Altitude.Enabled = false;
-			this.NumericUpDown_Fence_Altitude.Location = new System.Drawing.Point(143, 113);
+			resources.ApplyResources(this.NumericUpDown_Fence_Altitude, "NumericUpDown_Fence_Altitude");
 			this.NumericUpDown_Fence_Altitude.Max = 1F;
 			this.NumericUpDown_Fence_Altitude.Min = 0F;
 			this.NumericUpDown_Fence_Altitude.Name = "NumericUpDown_Fence_Altitude";
 			this.NumericUpDown_Fence_Altitude.ParamName = null;
-			this.NumericUpDown_Fence_Altitude.Size = new System.Drawing.Size(160, 22);
-			this.NumericUpDown_Fence_Altitude.TabIndex = 9;
 			// 
 			// label6
 			// 
-			this.label6.AutoSize = true;
-			this.label6.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			resources.ApplyResources(this.label6, "label6");
 			this.label6.ForeColor = System.Drawing.Color.White;
-			this.label6.Location = new System.Drawing.Point(3, 138);
 			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(101, 18);
-			this.label6.TabIndex = 10;
-			this.label6.Text = "RTL Altitude";
 			// 
 			// NumericUpDown_Fence_RTL_Altitude
 			// 
-			this.NumericUpDown_Fence_RTL_Altitude.Enabled = false;
-			this.NumericUpDown_Fence_RTL_Altitude.Location = new System.Drawing.Point(143, 141);
+			resources.ApplyResources(this.NumericUpDown_Fence_RTL_Altitude, "NumericUpDown_Fence_RTL_Altitude");
 			this.NumericUpDown_Fence_RTL_Altitude.Max = 1F;
 			this.NumericUpDown_Fence_RTL_Altitude.Min = 0F;
 			this.NumericUpDown_Fence_RTL_Altitude.Name = "NumericUpDown_Fence_RTL_Altitude";
 			this.NumericUpDown_Fence_RTL_Altitude.ParamName = null;
-			this.NumericUpDown_Fence_RTL_Altitude.Size = new System.Drawing.Size(160, 22);
-			this.NumericUpDown_Fence_RTL_Altitude.TabIndex = 11;
 			// 
 			// label1
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			resources.ApplyResources(this.label1, "label1");
 			this.label1.ForeColor = System.Drawing.Color.White;
-			this.label1.Location = new System.Drawing.Point(3, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(56, 18);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Enable";
 			// 
 			// label7
 			// 
-			this.label7.AutoSize = true;
-			this.label7.Font = new System.Drawing.Font("Georgia", 12F);
+			resources.ApplyResources(this.label7, "label7");
 			this.label7.ForeColor = System.Drawing.Color.White;
-			this.label7.Location = new System.Drawing.Point(39, 257);
 			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(120, 18);
-			this.label7.TabIndex = 1;
-			this.label7.Text = "Battery Falisafe";
 			// 
 			// tableLayoutPanel2
 			// 
-			this.tableLayoutPanel2.ColumnCount = 2;
-			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
-			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+			resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
 			this.tableLayoutPanel2.Controls.Add(this.label9, 0, 1);
 			this.tableLayoutPanel2.Controls.Add(this.label8, 0, 0);
 			this.tableLayoutPanel2.Controls.Add(this.NumericUpDown_LowBatt_Value, 1, 0);
 			this.tableLayoutPanel2.Controls.Add(this.ComboBox_Batt_Action, 1, 1);
-			this.tableLayoutPanel2.Location = new System.Drawing.Point(36, 291);
 			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-			this.tableLayoutPanel2.RowCount = 2;
-			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel2.Size = new System.Drawing.Size(350, 57);
-			this.tableLayoutPanel2.TabIndex = 2;
 			// 
 			// label9
 			// 
-			this.label9.AutoSize = true;
-			this.label9.Font = new System.Drawing.Font("Georgia", 12F);
+			resources.ApplyResources(this.label9, "label9");
 			this.label9.ForeColor = System.Drawing.Color.White;
-			this.label9.Location = new System.Drawing.Point(3, 28);
 			this.label9.Name = "label9";
-			this.label9.Size = new System.Drawing.Size(53, 18);
-			this.label9.TabIndex = 2;
-			this.label9.Text = "Action";
 			// 
 			// label8
 			// 
-			this.label8.AutoSize = true;
-			this.label8.Font = new System.Drawing.Font("Georgia", 12F);
+			resources.ApplyResources(this.label8, "label8");
 			this.label8.ForeColor = System.Drawing.Color.White;
-			this.label8.Location = new System.Drawing.Point(3, 0);
 			this.label8.Name = "label8";
-			this.label8.Size = new System.Drawing.Size(96, 18);
-			this.label8.TabIndex = 0;
-			this.label8.Text = "Low Battery";
 			// 
 			// NumericUpDown_LowBatt_Value
 			// 
-			this.NumericUpDown_LowBatt_Value.Enabled = false;
-			this.NumericUpDown_LowBatt_Value.Location = new System.Drawing.Point(143, 3);
+			resources.ApplyResources(this.NumericUpDown_LowBatt_Value, "NumericUpDown_LowBatt_Value");
 			this.NumericUpDown_LowBatt_Value.Max = 1F;
 			this.NumericUpDown_LowBatt_Value.Min = 0F;
 			this.NumericUpDown_LowBatt_Value.Name = "NumericUpDown_LowBatt_Value";
 			this.NumericUpDown_LowBatt_Value.ParamName = null;
-			this.NumericUpDown_LowBatt_Value.Size = new System.Drawing.Size(160, 22);
-			this.NumericUpDown_LowBatt_Value.TabIndex = 1;
 			// 
 			// ComboBox_Batt_Action
 			// 
+			resources.ApplyResources(this.ComboBox_Batt_Action, "ComboBox_Batt_Action");
 			this.ComboBox_Batt_Action.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.ComboBox_Batt_Action.Enabled = false;
 			this.ComboBox_Batt_Action.FormattingEnabled = true;
-			this.ComboBox_Batt_Action.Location = new System.Drawing.Point(143, 31);
 			this.ComboBox_Batt_Action.Name = "ComboBox_Batt_Action";
 			this.ComboBox_Batt_Action.ParamName = null;
-			this.ComboBox_Batt_Action.Size = new System.Drawing.Size(160, 20);
 			this.ComboBox_Batt_Action.SubControl = null;
-			this.ComboBox_Batt_Action.TabIndex = 3;
 			// 
 			// label10
 			// 
-			this.label10.AutoSize = true;
-			this.label10.Font = new System.Drawing.Font("Georgia", 12F);
+			resources.ApplyResources(this.label10, "label10");
 			this.label10.ForeColor = System.Drawing.Color.White;
-			this.label10.Location = new System.Drawing.Point(39, 24);
 			this.label10.Name = "label10";
-			this.label10.Size = new System.Drawing.Size(130, 18);
-			this.label10.TabIndex = 3;
-			this.label10.Text = "Simple GeoFence";
 			// 
 			// label11
 			// 
-			this.label11.AutoSize = true;
-			this.label11.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			resources.ApplyResources(this.label11, "label11");
 			this.label11.ForeColor = System.Drawing.Color.Red;
-			this.label11.Location = new System.Drawing.Point(39, 363);
 			this.label11.Name = "label11";
-			this.label11.Size = new System.Drawing.Size(341, 15);
-			this.label11.TabIndex = 4;
-			this.label11.Text = "Please restart system after adjusting for working properly";
 			// 
 			// ConfigGeoFencePage
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			resources.ApplyResources(this, "$this");
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
 			this.Controls.Add(this.label11);
@@ -331,7 +229,6 @@
 			this.Controls.Add(this.label7);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Name = "ConfigGeoFencePage";
-			this.Size = new System.Drawing.Size(433, 397);
 			this.tableLayoutPanel1.ResumeLayout(false);
 			this.tableLayoutPanel1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.NumericUpDown_Fence_Radius)).EndInit();
